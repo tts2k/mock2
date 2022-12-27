b06c73ecbcec2e2e4af5e468573c783b583a600d
tts2k <tranthaison2000@gmail.com>
2022-12-27 16:02:33 +0700

    product/dto: make search keyword and category queries optional

--- src/product/dto/get-all-products-query.dto.ts
+++ src/product/dto/get-all-products-query.dto.ts
@@ -1,13 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString } from 'class-validator'
+import { IsNumber, IsOptional, IsString } from 'class-validator'
 import { PagedQueryDto } from 'src/pagination/dto/paged-query.dto';
 
 export class GetAllProductsQueryDto extends PagedQueryDto {
   @ApiProperty({ required: false })
   @IsNumber()
+  @IsOptional()
   categoryId?: number;
 
   @ApiProperty({ required: false })
+  @IsOptional()
   @IsString()
   q?: string;
 }
