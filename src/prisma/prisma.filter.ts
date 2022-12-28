9020ff488f563ce6007054f6baabec00a9365c42
tts2k <tranthaison2000@gmail.com>
2022-12-28 15:16:14 +0700

    prisma/filter: handle P2025, P2004 and use switch statement

--- src/prisma/prisma.filter.ts
+++ src/prisma/prisma.filter.ts
@@ -7,28 +7,41 @@ export class PrismaFilter extends BaseExceptionFilter {
   catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse();
 
-    if (exception.code === 'P2001') {
-      response.status(HttpStatus.NOT_FOUND).json({
-        statusCode: HttpStatus.NOT_FOUND,
-        message: "Not found"
-      })
-    }
-    else if (exception.code === 'P2002') {
-      response.status(HttpStatus.BAD_REQUEST).json({
-        statusCode: HttpStatus.BAD_REQUEST,
-        message: "Duplicate value",
-        fields: exception.meta.target
-      })
-    }
-    else if (exception.code === 'P2003') {
-      response.status(HttpStatus.BAD_REQUEST).json({
-        statusCode: HttpStatus.BAD_REQUEST,
-        message: "Foreign key constraint error. Please check your foreign key id(s).",
-        field: exception.meta.field_name
-      })
-    }
-    else {
-      super.catch(exception, host);
+    switch(exception.code) {
+      case 'P2001':
+        response.status(HttpStatus.NOT_FOUND).json({
+          statusCode: HttpStatus.NOT_FOUND,
+          message: "Not found"
+        })
+        break;
+      case 'P2002':
+        response.status(HttpStatus.BAD_REQUEST).json({
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: "Duplicate value",
+          fields: exception.meta.target
+        })
+        break;
+      case 'P2003':
+        response.status(HttpStatus.BAD_REQUEST).json({
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: "Foreign key constraint error. Please check your foreign key id(s).",
+          field: exception.meta.field_name
+        })
+        break;
+      case 'P2004':
+        response.status(HttpStatus.BAD_REQUEST).json({
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: exception.meta.database_error
+        })
+        break;
+      case 'P2025':
+        response.status(HttpStatus.BAD_REQUEST).json({
+          statusCode: HttpStatus.BAD_REQUEST,
+          message: exception.meta.cause
+        })
+        break;
+      default:
+        super.catch(exception, host);
     }
   }
 }
